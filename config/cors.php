0831eea6220a3b29918afb92dd5a3405cd86dfdc
Huttunenbe <beniittahuttunen@gmail.com>
2025-07-16 22:40:26 +0300

    change url in cors to match render

--- config/cors.php
+++ config/cors.php
@@ -4,15 +4,15 @@ return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'http://localhost:5174'
+        'http://localhost:5173',
+        'https://your-frontend.onrender.com',
     ],
-   
+
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,  
- 
+    'supports_credentials' => true,
+
 
 ];
-
